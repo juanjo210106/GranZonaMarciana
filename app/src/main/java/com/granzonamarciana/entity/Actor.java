4337d2db37aa327014d6975042f348ade22149cb
juanjo210106 <jgamlop2101@g.educaand.es>
2026-01-25 17:28:40 +0100

    Actualización en la clase Actor.java

--- app/src/main/java/com/granzonamarciana/entity/Actor.java
+++ app/src/main/java/com/granzonamarciana/entity/Actor.java
@@ -8,6 +8,7 @@ public class Actor {
     @PrimaryKey(autoGenerate = true)
     public int id;
 
+    // Datos personales
     public String nombreUsuario;
     public String contrasena;
     public String nombre;
@@ -15,19 +16,90 @@ public class Actor {
     public String email;
     public String telefono;
 
-    // Las imágenes son enlaces
+    // Imágenes como enlaces
     public String imagenUrl;
 
     // Roles: "ADMIN", "CONCURSANTE", "ESPECTADOR"
     public String rol;
 
+    // Constructor vacío
     public Actor() {
     }
 
-    public Actor(String nombreUsuario, String contrasena, String nombre, String rol) {
+    // Constructor completo
+    public Actor(String nombreUsuario, String contrasena, String nombre, String apellidos, String email, String telefono, String imagenUrl, String rol) {
         this.nombreUsuario = nombreUsuario;
         this.contrasena = contrasena;
         this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+        this.imagenUrl = imagenUrl;
+        this.rol = rol;
+    }
+
+    // Getters y Setters
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
         this.rol = rol;
     }
 }
