c065cd91ed07ef7ae80a1244da7afbf8d454f7b8
José Manuel Jiménez Rodríguez <josemajimenezrodriguez8@gmail.com>
2026-01-28 21:01:38 +0100

    Creación de item_edicion.xml y EdicionAdapter

---
+++ app/src/main/java/com/granzonamarciana/adapter/EdicionAdapter.java
@@ -0,0 +1,53 @@
+package com.granzonamarciana.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.granzonamarciana.R;
+import com.granzonamarciana.entity.Edicion;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class EdicionAdapter extends ArrayAdapter<Edicion> {
+
+    public EdicionAdapter(@NonNull Context context, @NonNull List<Edicion> ediciones) {
+        super(context, 0, ediciones);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Edicion edicion = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.item_edicion, parent, false);
+        }
+
+        TextView tvTituloEdicion = convertView.findViewById(R.id.tvTituloEdicion);
+        TextView tvFechas = convertView.findViewById(R.id.tvFechas);
+        TextView tvParticipantes = convertView.findViewById(R.id.tvParticipantes);
+
+        if (edicion != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            tvTituloEdicion.setText("Edición " + edicion.getId());
+
+            String inicio = edicion.getFechaInicio() != null ? edicion.getFechaInicio().format(formatter) : "";
+            String fin = edicion.getFechaFin() != null ? edicion.getFechaFin().format(formatter) : "";
+
+            tvFechas.setText("Inicio: " + inicio + " - Fin: " + fin);
+            tvParticipantes.setText("Max. Participantes: " + edicion.getMaxParticipantes());
+        }
+
+        return convertView;
+    }
+}
