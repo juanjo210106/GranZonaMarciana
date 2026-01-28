d33f5f432177ed7fe93805f8d566c0a387a69f1e
rafald10 <rafalazarodiaz@gmail.com>
2026-01-28 16:50:27 +0100

    Puntucion arreglado y creacion de noticia

---
+++ app/src/main/java/com/granzonamarciana/entity/Noticia.java
@@ -0,0 +1,63 @@
+package com.granzonamarciana.entity;
+
+import androidx.room.Entity;
+import androidx.room.TypeConverters;
+
+import com.granzonamarciana.database.LocalDateTimeConverter;
+
+import java.time.LocalDateTime;
+
+@Entity(tableName = "noticia")
+@TypeConverters({LocalDateTimeConverter.class})
+public class Noticia extends DomainEntity {
+
+    private LocalDateTime fechaPublicacion;
+    private String cabecera;
+    private String cuerpo;
+    private String urlImagen;
+
+    // Constructor
+    public Noticia(LocalDateTime fechaPublicacion, String cabecera, String cuerpo, String urlImagen) {
+        this.fechaPublicacion = fechaPublicacion;
+        this.cabecera = cabecera;
+        this.cuerpo = cuerpo;
+        this.urlImagen = urlImagen;
+    }
+
+    // Getters y Setters
+    public LocalDateTime getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDateTime fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public String getCabecera() {
+        return cabecera;
+    }
+
+    public void setCabecera(String cabecera) {
+        this.cabecera = cabecera;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public String getUrlImagen() {
+        return urlImagen;
+    }
+
+    public void setUrlImagen(String urlImagen) {
+        this.urlImagen = urlImagen;
+    }
+}
+
+
+
+
--- app/src/main/java/com/granzonamarciana/entity/Puntuacion.java
+++ app/src/main/java/com/granzonamarciana/entity/Puntuacion.java
@@ -3,14 +3,12 @@ package com.granzonamarciana.entity;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 
-import com.granzonamarciana.entity.DomainEntity;
-
 @Entity(tableName = "puntuacion")
 public class Puntuacion extends DomainEntity {
 
     private int valor;
 
-    // Relaciones mediante IDs (Estilo de tu maestro en Anuncio.java)
+    // Relaciones mediante IDs
     @ColumnInfo(name = "idEspectador")
     private int idEspectador;
 
@@ -25,6 +23,39 @@ public class Puntuacion extends DomainEntity {
         this.valor = valor;
         this.idEspectador = idEspectador;
         this.idConcursante = idConcursante;
+        this.idGala = idGala;
+    }
+
+    // Getters y Setters
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getIdEspectador() {
+        return idEspectador;
+    }
+
+    public void setIdEspectador(int idEspectador) {
+        this.idEspectador = idEspectador;
+    }
+
+    public int getIdConcursante() {
+        return idConcursante;
+    }
+
+    public void setIdConcursante(int idConcursante) {
+        this.idConcursante = idConcursante;
+    }
+
+    public int getIdGala() {
+        return idGala;
+    }
+
+    public void setIdGala(int idGala) {
         this.idGala = idGala;
     }
 }
